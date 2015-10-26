4f5903e716ead1d852f1b592d31ecdb202042fd1
supermomonga <hi@supermomonga.com>
2015-10-26 21:22:51 +0900

    cheshire

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [emiln/slacker "1.5.1"]
                  [environ "1.0.0"]
+                 [cheshire "5.5.0"]
                  [robert/hooke "1.3.0"]]
   :plugins [[lein-environ "1.0.0"]]
   :profiles {:uberjar {:aot :all}}
