c2cd0e982bb5597ae592746a606985f264339413
supermomonga <hi@supermomonga.com>
2015-10-26 21:08:00 +0900

    ppp

--- src/omochi/core.clj
+++ src/omochi/core.clj
@@ -2,7 +2,9 @@
   (:require [clojure.string :refer [lower-case]]
             [slacker.client :refer [emit! await! handle with-stacktrace-log] :as s]
             [clojure.tools.logging :as log]
-            [environ.core :refer [env]]))
+            [environ.core :refer [env]]
+            [cheshire.core :refer [parse-stream]]
+            [clojure.java.io :as io]))
 
 (defn yamabiko
   [{:keys [channel user text]}]
@@ -16,7 +18,16 @@
   (when (= text "ping")
    (emit! :slacker.client/send-message channel "pong")))
 
+(defn ppp
+  [{:keys [channel user text]}]
+  (when-let [text (last (re-find #"ppp" text))]
+    (emit! :slacker.client/send-message channel "PonPonPain")))
+
+(defn idols []
+  (-> "id2hash.json" io/resource io/reader))
+
 (defn run []
+  (handle :message ppp)
   (handle :message ping-pong)
   (handle :message yamabiko)
   (if-let [api-token (env :slack-api-token)]
