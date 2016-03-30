472d7c1eebe5c39edbeacd9977160a468bd74740
supermomonga <hi@supermomonga.com>
2016-03-31 01:13:47 +0900

    oh

--- src/omochi/core.clj
+++ src/omochi/core.clj
@@ -64,19 +64,19 @@
              (format "<@%s>: %s" user text)))))
 
 (def simple-matcher-rules
-  {#"^ping"                      "pong"
-   #"^目の錯覚(って怖いねん)?"   "http://i.gyazo.com/552f4577e9bb63c18766a705fc63f553.jpg"
-   #"^目の錯覚(って怖いですね)?" "http://i.gyazo.com/4383943b54188d8bcf185456516186b8.jpg"
-   #"^!ppp"                      "PonPonPain"
-   #"^!b"                        "便利"
-   #"^!bs"                       "便利そう"
-   #"^!f"                        "不便"
-   #"^!fs"                       "不便そう"
-   #"^!no"                       "http://d.pr/i/15zJh.png"
-   #"^ぬるオーラ"                "http://d.pr/i/15zJh.png"
-   #"^行けたら行く"              "http://d.pr/i/11Q6l.png"
-   #"^!snttm"                    "http://d.pr/i/11H8B.png"
-   #"^汁なし担々麺"              "http://d.pr/i/11H8B.png"})
+  {#"^ping"                       "pong"
+   #"^目の錯覚(って怖いねん)?$"   "http://i.gyazo.com/552f4577e9bb63c18766a705fc63f553.jpg"
+   #"^目の錯覚(って怖いですね)?$" "http://i.gyazo.com/4383943b54188d8bcf185456516186b8.jpg"
+   #"^!ppp"                       "PonPonPain"
+   #"^!b"                         "便利"
+   #"^!bs"                        "便利そう"
+   #"^!f"                         "不便"
+   #"^!fs"                        "不便そう"
+   #"^!no"                        "http://d.pr/i/15zJh.png"
+   #"^ぬるオーラ"                 "http://d.pr/i/15zJh.png"
+   #"^行けたら行く"               "http://d.pr/i/11Q6l.png"
+   #"^!snttm"                     "http://d.pr/i/11H8B.png"
+   #"^汁なし担々麺"               "http://d.pr/i/11H8B.png"})
 
 (defn simple-matcher [text rules]
   (first (when-let  [rules (filter #(-> % key (re-find text)) rules) ]
