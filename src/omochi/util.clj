491001f264d74ecc77fdee00e43fb721ac606d77
supermomonga <hi@supermomonga.com>
2016-04-16 17:51:38 +0900

    fix image url matching

--- src/omochi/util.clj
+++ src/omochi/util.clj
@@ -7,7 +7,7 @@
 
 (defn ensure-fresh-image [text]
   (and text
-       (clojure.string/replace text #"(https?:\/\/(?:.+?)\.(?:jpg|jpeg|png|gif))" #(append-timestamp (last %)))))
+       (clojure.string/replace text #"(https?:\/\/(?:[^ ]+)\.(?:jpg|jpeg|png|gif))" #(append-timestamp (last %)))))
 
 (defn mention-to? [id text]
   (when-let [[_ to] (re-find #"^<@([^>]+)>:?\s+" text) ]
