a976bc491181af77be932ce0b44dc5b8fe1a09c3
supermomonga <hi@supermomonga.com>
2017-06-07 16:28:19 +0900

    fix bug

--- src/omochi/handler/tofu.clj
+++ src/omochi/handler/tofu.clj
@@ -3,15 +3,15 @@
   (:require [omochi.util :as util]
             [slacker.client :refer [emit!]]))
 
-(defn contain-tofu? [text]
-  (clojure.string/includes? "\b"))
+(defn include-tofu? [text]
+  (clojure.string/includes? text "\b"))
 
 (defn remove-tofu [text]
   (clojure.string/replace text "\b" ""))
 
 (defn handler
   [{:keys [channel user text]}]
-  (when (contain-tofu? text)
+  (when (include-tofu? text)
     (let [response (remove-tofu text)]
         (emit! :slacker.client/send-message channel
            (util/ensure-fresh-image response)))))
